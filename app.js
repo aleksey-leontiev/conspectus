94531004065c1fe5e09bcab82caa2f2fa59bf44e
Aleksey Leontiev <aleontiev@MacBook-Pro-Aleksey.local>
2016-06-12 15:12:02 +0300

    initial sources

---
+++ app.js
@@ -0,0 +1,33 @@
+const electron = require('electron')
+const isDev = require('electron-is-dev');
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.loadURL(`file://${__dirname}/app/dashboard/view.html`)
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools()
+  }
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
